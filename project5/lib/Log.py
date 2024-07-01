49f302e782bafcf5787f2b820878e2c5b7214a20
callybs <callysobrinho@gmail.com>
2024-07-01 16:56:38 -0400

    Delete project5 directory

--- project5/lib/Log.py
+++
@@ -1,9 +0,0 @@
-class Log:
-    
-    @staticmethod 
-    def info(message: str):
-        print(f'INFO: {message}', flush=True)
-
-    @staticmethod
-    def error(message: str):
-        print(f'ERROR: {message}', flush=True)
